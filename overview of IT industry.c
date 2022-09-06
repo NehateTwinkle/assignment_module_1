070934d209cf1ff45c3e91ff025e973fbe0a3e49
NehateTwinkle <110092490+NehateTwinkle@users.noreply.github.com>
2022-09-06 23:10:26 +0530

    Add files via upload

---
+++ overview of IT industry.c
@@ -0,0 +1,28 @@
+                              OVERVIWE  OF IT INDUSTY
+            
+    What is meaning by it industry?
+=> IT sector incude companies that produce software,hardware or semiconductore 
+equipment,and companies that provided internet or related services.
+
+=> INFORMATION TECHNOLOGY (IT) is the use of computers and software to manage information.
+
+=>SOFTWARE AND SERVICES
+
+ ==>   The software and services industry group is made up of comapies that provide
+	 internet services ,as well as companies that provide software and IT services.
+	 Internet services incude companies that provide online databases or interactive 
+	 services,such as search engines or social networks. IT services includes companies 
+	 that provide IT consulting or data processing services to other companies.Finally,
+	 software consists of any sort of software for business or consumer use,ranging from
+	  enterprise software any systems software to video games.
+	  
+=>	TECHNOLOGY HARDWARE AND EQUIPMENT
+
+     Tchnology hardware and equipment is broken down into three industries;communications
+	 equipment; technology hardware,storage and peripherals;and electonic equipment,
+	 instruments and components.
+	 
+=> SEMICONDUCTORES AND SENICONDUCTOR EQUIPMENT
+
+    Semiconductors and substances that can conduct electricity under some conditions,
+	but not others, making them ideal for controlling electrical currents.2   
